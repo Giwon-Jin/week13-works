0adfcfdd546870c25ea8be3247a51d8083265b37
Giwon-Jin <71603761+Giwon-Jin@users.noreply.github.com>
2020-12-07 06:17:45 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,75 @@
+$(document).ready(function(){
+
+	$('.yvd').click(function(){
+
+	    $('.title').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_01').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_02').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_03').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_04').css({ 
+    	'display':'none'
+		});
+
+		$('.sub_05').css({ 
+    	'display':'none'
+		});
+
+		$('.yvd').css({ 
+    	'display':'none'
+		});
+
+		$('.background').css({ 
+    	'display':'flex'
+		});
+
+		$('.title_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_01_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_02_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_03_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_04_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.sub_05_B').css({ 
+    	'display':'inline-block'
+		});
+
+		$('.yvd_B').css({ 
+    	'display':'inline-block'
+		});
+
+
+
+
+	})
+
+
+})
+
+
+        
+
